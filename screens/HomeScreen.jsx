e1ee9263505b6508d800fc7214203281dfa33148
PJaworski <pjaworski.dev@gmail.com>
2024-08-30 15:29:28 +0200

    build: upd. home screen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,35 +1,30 @@
 import React from 'react'
 import { View, StyleSheet, Dimensions } from 'react-native';
-import { Header, HomeTask } from '../components';
+import { Header, HomeTask, BaseView } from '../components';
 import { FlatList, TouchableOpacity } from 'react-native';
-
 
 
 function HomeScreen({navigation}) {
   return (
-    <View style={styles.container}>
+    <BaseView>
       <Header {...{navigation}} />
       <FlatList
         contentContainerStyle={styles.listContent}
         data={[1, 2, 3, 3]}
         renderItem={({ item }) =>
           <TouchableOpacity onPress={() => navigation.navigate('showdetailstask', { id: item.id, title: item.title })}>
-            <HomeTask />
+            <HomeTask text="Home Task" />
           </TouchableOpacity>
         }
       />
 
       
-    </View>
+    </BaseView>
   );
 }
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#eaf2f7',
-  },
   listContent: {
     minHeight: Dimensions.get('window').height - 100
   }
