cc9e909d745b7a43127d8ccc8c9f611c0e2f7377
PJaworski <pjaworski.dev@gmail.com>
2024-08-30 15:30:23 +0200

    minor

--- components/HomeTask.jsx
+++ components/HomeTask.jsx
@@ -1,24 +1,11 @@
-import { View, Text } from 'react-native'
+import { Text } from 'react-native'
 import React from 'react'
-import styled from 'styled-components/native'
+import { LineItemView } from '.'
 
-export const HomeTask = () => {
+export const HomeTask = ({text}) => {
   return (
-    <PostView>
-      <Text>HomeTask</Text>
-    </PostView>
+    <LineItemView>
+      <Text>{text}</Text>
+    </LineItemView>
   )
 }
-
-
-const PostView = styled.View`
-padding: 10px;
-align-items:center;
-background-color: #ffffff;
-min-height: 55px;
-min-width: 100%;
-border-radius: 0;
-margin: 5px 0 0;
-flex-direction: row;
-border: 1px solid #dde7ee;
-`
