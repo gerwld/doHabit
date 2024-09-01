9efcbbaacd978f10bdf022e984695ccd6eaeff3b
PJaworski <pjaworski.dev@gmail.com>
2024-09-01 14:26:04 +0200

    build: updated home screen(lastsevendays)

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -2,26 +2,32 @@ import React from 'react'
 import { View, StyleSheet, Dimensions } from 'react-native';
 import { Header, HomeTask, BaseView } from '../components';
 import { FlatList, TouchableOpacity } from 'react-native';
+import LastSevenDays from '../components/LastSevenDays';
 
 
-function HomeScreen({navigation}) {
+function HomeScreen({ navigation }) {
   return (
     <BaseView>
-      <Header {...{navigation}} />
-      <FlatList
-        contentContainerStyle={styles.listContent}
-        data={[1, 2, 3, 3]}
-        renderItem={({ item }) =>
-          <TouchableOpacity onPress={() => navigation.navigate('showdetailstask', { id: item.id, title: item.title })}>
-            <HomeTask text="Home Task" />
-          </TouchableOpacity>
-        }
-      />
+      <Header {...{ navigation }} />
+      <LastSevenDays/>
+      <LatestTasks />
 
-      
     </BaseView>
   );
 }
+
+const LatestTasks = () => (
+  <FlatList
+    contentContainerStyle={styles.listContent}
+    data={[1, 2, 3, 3]}
+    renderItem={({ item }) =>
+      <TouchableOpacity onPress={() => navigation.navigate('showdetailstask', { id: item.id, title: item.title })}>
+        <HomeTask text="Home Task" />
+      </TouchableOpacity>
+    }
+  />
+
+)
 
 
 const styles = StyleSheet.create({
