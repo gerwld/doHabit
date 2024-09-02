7680125eb3ca6ca265ea33fe19776c5aada138e5
PJaworski <pjaworski.dev@gmail.com>
2024-09-02 22:21:00 +0200

    build: added i18n titles

--- screens/Navigation.jsx
+++ screens/Navigation.jsx
@@ -4,18 +4,20 @@ import HomeScreen from "./HomeScreen";
 import AddTaskScreen from "./AddTaskScreen";
 import ShowDetailsTaskScreen from "./ShowDetailsTaskScreen";
 import SettingsScreen from "./SettingsScreen";
+import { useTranslation } from "react-i18next";
 
 
 const Stack = createNativeStackNavigator();
 
 export const Navigation = () => {
+    const {t} = useTranslation();
     return (
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="home" component={HomeScreen}  options={{ headerShown: false }} />
-                <Stack.Screen name="addtask" component={AddTaskScreen}  options={{ headerShown: true, title: "Add Task" }} />
+                <Stack.Screen name="addtask" component={AddTaskScreen}  options={{ headerShown: true, title: t("addt_screen") }} />
                 <Stack.Screen name="showdetailstask" component={ShowDetailsTaskScreen}  options={{ headerShown: true, title: "Habit Details" }} />
-                <Stack.Screen name="settings" component={SettingsScreen}  options={{ headerShown: true, title: "Settings" }} />
+                <Stack.Screen name="settings" component={SettingsScreen}  options={{ headerShown: true, title: t("st_screen") }} />
             </Stack.Navigator>
         </NavigationContainer>
     )
