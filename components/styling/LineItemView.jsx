509a2a8ecff05547f0605c0c513549ef3bb5961c
PJaworski <pjaworski.dev@gmail.com>
2024-09-02 21:51:40 +0200

    build: added Switch (settings screen)

--- components/styling/LineItemView.jsx
+++ components/styling/LineItemView.jsx
@@ -1,10 +1,10 @@
 import { Icon } from '@rneui/base';
 import React from 'react'
-import { Text } from 'react-native';
+import { Switch, Text } from 'react-native';
 import styled from "styled-components/native";
 
 
- const LineItemViewItem = styled.View`
+const LineItemViewItem = styled.View`
 padding: 10px 0px 10px 15px;
 align-items:center;
 justify-content:space-between;
@@ -17,10 +17,20 @@ flex-direction: row;
 border: 1px solid #dde7ee;
 `
 
-export const LineItemView = ({children, rightArrow}) => (
+export const LineItemView = ({ children, rightArrow, toggle, onToggle, isEnabled }) => (
     <LineItemViewItem>
         {children}
-        {rightArrow ? <Icon style={{marginRight: 10}} type="entypo" size={21} name="chevron-thin-right" color="#ccd1db" /> : ""}
+        {rightArrow ? <Icon style={{ marginRight: 10 }} type="entypo" size={21} name="chevron-thin-right" color="#ccd1db" /> : ""}
+        {toggle ?
+                <Switch
+                    style={{ marginRight: 10 }}
+                    trackColor={{ false: '#b1b1b1', true: '#81b0ff' }}
+                    thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                    ios_backgroundColor="#968d8d"
+                    onValueChange={onToggle ? onToggle : null}
+                    value={isEnabled ? isEnabled : null}
+                />
+            : ""}
     </LineItemViewItem>
 )
 
