ce0a7d192af43b65f2f5e5a0bf9d1792e5491a61
PJaworski <pjaworski.dev@gmail.com>
2024-09-05 21:20:47 +0200

    build: upd. nav

--- src/screens/Navigation.jsx
+++ src/screens/Navigation.jsx
@@ -2,10 +2,11 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import HomeScreen from "./HomeScreen";
 import AddHabitScreen from "./AddHabitScreen";
-import SettingsScreen from "./SettingsScreen";
 import { useTranslation } from "react-i18next";
 import TutorialScreen from "./TutorialScreen";
-import HabitDetailsScreen from "./HabitDetailsScreen";
+import DetailsHabitScreen from "./DetailsHabitScreen";
+import SettingsScreen from "./SettingsScreen";
+import EditHabitScreen from "./EditHabitScreen";
 
 
 const Stack = createNativeStackNavigator();
@@ -17,13 +18,14 @@ export const Navigation = () => {
     navTheme.colors.background = '#f2f2f7';
 
     return (
-        <NavigationContainer theme={navTheme}>
+        <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="home" component={HomeScreen}  options={{ headerShown: false, title: t("home_screen") }} />
                 <Stack.Screen name="tutorial" component={TutorialScreen}  options={{ headerShown: false, title: t("tutorial") }} />
                 <Stack.Screen name="settings" component={SettingsScreen}  options={{ headerShown: false, title: t("st_screen") }} />
                 <Stack.Screen name="addhabit" component={AddHabitScreen}  options={{ headerShown: false, title: t("addt_screen") }} />
-                <Stack.Screen name="habitdetails" component={HabitDetailsScreen}  options={{ headerShown: true, title: "Habit Details" }} />
+                <Stack.Screen name="edithabit" component={EditHabitScreen}  options={{ headerShown: false, title: t("addt_screen") }} />
+                <Stack.Screen name="habitdetails" component={DetailsHabitScreen}  options={{ headerShown: false, title: "Habit Details" }} />
                 
             </Stack.Navigator>
         </NavigationContainer>
