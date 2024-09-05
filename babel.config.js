986573dc9dca9ada0e547a6d6f9c17e30b30254d
PJaworski <pjaworski.dev@gmail.com>
2024-09-05 15:35:43 +0200

    build: added alises for babel conf

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            components: './src/components',
+            screens: './src/screens',
+            locales: './src/locales',
+            redux: './src/redux',
+            actions: './src/redux/actions',
+            hoc: './src/hoc',
+          },
+        },
+      ],
+    ],
+  }
+}
