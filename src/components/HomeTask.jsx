25d0e91c951bccc6dad8102707d9baae1e79054e
PJaworski <pjaworski.dev@gmail.com>
2024-09-05 21:19:06 +0200

    update: HomeTask

--- src/components/HomeTask.jsx
+++ src/components/HomeTask.jsx
@@ -1,16 +1,20 @@
-import { Pressable, Text } from 'react-native'
+import { Text, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { LastSevenDays, CircularProgress, LineItemView } from 'components'
+import LineItemView from './styling/LineItemView'
+import { LastSevenDays } from './LastSevenDays'
+import CircularProgress from './CircularProgress'
+import { useNavigation } from '@react-navigation/native'
 
-export const HomeTask = ({ name, navigation }) => {
 
+export const HomeTask = ({ name }) => {
+  const navigation = useNavigation();
   return (
-    <Pressable onPress={() => navigation.navigate("habitdetails")}>
+    <TouchableOpacity onPress={() => navigation.navigate("habitdetails")} >
       <LineItemView>
         <CircularProgress progress={20} size={22} strokeWidth={5} color="#7fcbfd" />
         <Text style={{ flex: 1, marginLeft: 10, marginRight: 5, color: "#50677a" }}>{name}</Text>
         <LastSevenDays isTask />
       </LineItemView>
-    </Pressable>
+    </TouchableOpacity>
   )
 }
