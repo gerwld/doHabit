6802965c69942ec27ce5922ae68e807416e9db28
PJaworski <pjaworski.dev@gmail.com>
2024-09-07 10:24:19 +0200

    added LineItemOptions export

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { LastSevenDays } from "./LastSevenDays";
 
 export { BaseView } from "./styling/BaseView";
 export { default as LineItemView } from "./styling/LineItemView";
+export { default as LineItemOptions } from "./LineItemOptions";
 export { GapView } from "./styling/GapView";
 export { HomeTask } from "./HomeTask";
 export { default as CircularProgress } from "./CircularProgress";
