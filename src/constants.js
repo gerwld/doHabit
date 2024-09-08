c177b438696d4dd8a8d1bd0cc7287dfd3c2e2915
PJaworski <pjaworski.dev@gmail.com>
2024-09-08 14:49:54 +0200

    added THEMEV, getTheme, getThemeStatusBar

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,27 @@ const THEMES_MASKS = {
     "st_theme__system": "System",
 }
 
+const THEMEV = {
+    "st_theme__light": {
+        background: "#f0f2f7",
+        bgHighlight: "#ffffff",
+        borderColor: "#e5e5ea",
+        textColor: "#50677a",
+        textColorHighlight: "#000",
+        textColorRevert: "#fff",
+        crossSymb: "#a5bbd3"
+    },
+    "st_theme__dark": {
+        background: "#000000",
+        bgHighlight: "#242424",
+        borderColor: "#000",
+        textColor: "#c0cdd9",
+        textColorHighlight: "#fff",
+        textColorRevert: "#fff",
+        crossSymb: "#45474b",
+    }
+}
+
 const REPEAT_MASKS = {
     "every-day": "Every Day",
     "every-week": "Every Week",
@@ -34,6 +55,32 @@ const getRandomItem = (arr) => {
     return arr[randomIndex];
 };
 
+const getTheme = (theme) => {
+    if (theme === "st_theme__dark") return THEMEV["st_theme__dark"]
+    if (theme === "st_theme__light") return THEMEV["st_theme__light"]
+    return THEMEV["st_theme__light"]
+}
+
+const getThemeStatusBar = (theme, isReversed) => {
+    if (isReversed) switch (theme) {
+        case "st_theme__dark":
+            return "light-content"
+        case "st_theme__light":
+            return "dark-content"
+        default:
+            return "dark-content"
+    }
+
+    else switch (theme) {
+        case "st_theme__dark":
+            return "dark-content"
+        case "st_theme__light":
+            return "light-content"
+        default:
+            return "light-content"
+    }
+}
+
 module.exports = {
-    THEMES_MASKS, REPEAT_MASKS, LANG_MASKS, HABIT_COLORS, getRandomItem
+    THEMES_MASKS, REPEAT_MASKS, LANG_MASKS, HABIT_COLORS, THEMEV, getRandomItem, getTheme, getThemeStatusBar
 }
