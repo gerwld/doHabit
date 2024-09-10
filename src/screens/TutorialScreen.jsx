d4230e3610ec95302ab679bc470d23a875e767a9
PJaworski <pjaworski.dev@gmail.com>
2024-09-10 18:37:26 +0200

    mv ScrollPages from TutorialScreen, pt2

--- src/screens/TutorialScreen.jsx
+++ src/screens/TutorialScreen.jsx
@@ -1,131 +1,50 @@
-import { View, Text, Button, SafeAreaView, Dimensions } from 'react-native'
 import React from 'react'
-import { Gesture, GestureDetector, GestureHandlerRootView } from 'react-native-gesture-handler';
-import { useAnimatedStyle, withTiming } from 'react-native-reanimated';
-import Animated, { useSharedValue } from 'react-native-reanimated';
-const AnimatedView = Animated.createAnimatedComponent(View);
-import styled from 'styled-components/native';
+import { View, Text, StyleSheet } from 'react-native'
+import { getTheme } from '@constants';
+import {ScrollPages} from '@components'
+import { useSelector } from 'react-redux'
 
-const TutorialScreen = ({ navigation }) => {
-    const windowWidth = Dimensions.get('window').width;
-    const total_pages = 2;
+const TutorialScreen = () => {
+    const { theme} = useSelector(({app}) => ({theme: app.theme}))
+    const styles = StyleSheet.create({
+        screen: {
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+        },
+        title: {
+            fontSize: 24,
+            fontWeight: "bold",
+            textAlign: "center",
+            color: getTheme(theme).textColor,
+            marginBottom: 10
+        },
+        description: {
+            fontSize: 15,
+            color: "gray",
+            textAlign: "center"
+        }
+    })
 
-    const x = useSharedValue(0);
-    const page = useSharedValue(0)
-    const startX  = useSharedValue()
+  return (
+    <ScrollPages>
+        <View style={styles.screen}>
+            <Text style={styles.title}>Title</Text>
+            <Text style={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae magni similique laudantium commodi animi dignissimos aspernatur aliquid, ipsum beatae cupiditate.</Text>
+        </View>
 
-    const pan = Gesture.Pan()
-        .onBegin((event) => {
-            startX.value  = event.translationX;
-        })
-        .onChange((event) => {
-            x.value = -(page.value * windowWidth) + event.translationX
-        })
-        .onFinalize((event) => {
-            // prevent on tiny scroll
-            if (Math.max(startX.value , event.translationX) - Math.min(startX.value , event.translationX) > 10) {
-                // back
-                if (startX.value  < event.translationX && page.value >= 1) {
-                    x.value = withTiming(-windowWidth * (page.value - 1))
-                    page.value = page.value - 1;
-                }
-                // forward   
-                else if (startX.value  > event.translationX && page.value < total_pages) {
-                    x.value = withTiming(-windowWidth * (page.value + 1))
-                    page.value = page.value + 1;
-                }
-                else x.value = withTiming(-windowWidth * (page.value))
+        <View style={styles.screen}>
+            <Text style={styles.title}>Title 2</Text>
+            <Text style={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae magni similique laudantium commodi animi dignissimos aspernatur aliquid, ipsum beatae cupiditate.</Text>
+        </View>
 
-            }
-            else x.value = withTiming(-windowWidth * (page.value))
-
-            startX.value  = 0
-        });
-
-    const animatedContainerStyle = useAnimatedStyle(() => ({
-        transform: [{ translateX: x.value }]
-    }))
-
-    const styles = {}
-
-
-    return (
-
-        <SafeAreaView style={{ flex: 1 }}>
-            <GestureHandlerRootView>
-            <View style={{ flex: 1, width: windowWidth, overflow: "hidden" }}>
-                <Animated.View style={[{
-                    flex: 1,
-                    flexDirection: "row",
-                }, animatedContainerStyle]}>
-                    <Slide
-                        style={{
-
-                            backgroundColor: 'violet',
-                        }}
-                    >
-                        <Text>page.value 1 {windowWidth}</Text>
-                    </Slide>
-                    <Slide
-                        style={{
-
-                            backgroundColor: 'green',
-                        }}
-                    >
-                        <Text>page.value 2</Text>
-                    </Slide>
-                    <Slide
-                        style={{
-
-                            backgroundColor: 'blue',
-                        }}
-                    >
-                        <Text>page.value 3</Text>
-                    </Slide>
-
-                </Animated.View>
-
-                <GestureDetector gesture={pan}>
-                    <AnimatedView style={{
-                        backgroundColor: "rgba(255, 255, 255, 0.169)",
-                        position: "absolute",
-                        left: 0,
-                        top: 0,
-                        right: 0,
-                        bottom: 0,
-                    }}></AnimatedView>
-                </GestureDetector>
-
-            </View>
-            <View style={{ width: "max-width", alignItems: "center" }}>
-                <View style={{ width: 100 }}>
-                    <Button onPress={() => navigation.navigate("home")} title="Skip" />
-                </View>
-            </View>
-            <View style={{ paddingVertical: 10, flexDirection: "row", gap: 15, justifyContent: "center" }}>
-                <PageButton onPress={() => { page.value = 0; x.value = withTiming(-windowWidth * 0) }}><Text style={{ color: "white" }}>1</Text></PageButton>
-                <PageButton onPress={() => { page.value = 1; x.value = withTiming(-windowWidth * 1) }}><Text style={{ color: "white" }}>2</Text></PageButton>
-                <PageButton onPress={() => { page.value = 2; x.value = withTiming(-windowWidth * 2) }}><Text style={{ color: "white" }}>3</Text></PageButton>
-            </View>
-            </GestureHandlerRootView>
-        </SafeAreaView>
-    )
+        <View style={styles.screen}>
+            <Text style={styles.title}>Title 3</Text>
+            <Text style={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae magni similique laudantium commodi animi dignissimos aspernatur aliquid, ipsum beatae cupiditate.</Text>
+        </View>
+    </ScrollPages>
+  )
 }
-
-const PageButton = styled.Pressable`
-    width: 30px;
-    height: 30px;
-    align-items:center;
-    justify-content:center;
-    background-color: #8080804f;
-    color: black;
-    border-radius: 50%;
-`
-
-const Slide = styled.Pressable`
- width: 100%;
- height: 100%;
-   padding: 10px;
-`
 
 export default TutorialScreen
