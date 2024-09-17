5d44b4a2183a067725709804acba0725b52f7747
P Jaworski <pjaworski.dev@gmail.com>
2024-09-18 01:39:42 +0200

    refactored header without using HeaderRNE

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,86 +1,96 @@
-import React from 'react'
-import { View, Pressable, StyleSheet, Text } from 'react-native'
-import { Header as HeaderRNE, Icon } from '@rneui/themed';
+import React from 'react';
+import { View, Pressable, StyleSheet, Text } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { Icon } from '@rneui/themed';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
+const HEADER_HEIGHT_SAFE = 55;
 
 export const Header = React.memo(({ navigation, leftChild, rightChild }) => {
+    const insets = useSafeAreaInsets();
+
+    const styles = StyleSheet.create({
+        header: {
+            width: "100%",
+            height: insets.top + HEADER_HEIGHT_SAFE,
+            maxHeight: insets.top + HEADER_HEIGHT_SAFE,
+            minHeight: insets.top + HEADER_HEIGHT_SAFE,
+            padding: 0,
+            alignItems: "center",
+            borderBottomWidth: 0,
+        },
+        headerContent: {
+            width: "100%",
+            height: HEADER_HEIGHT_SAFE,
+            maxHeight: HEADER_HEIGHT_SAFE,
+            minHeight: HEADER_HEIGHT_SAFE,
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: "flex-start",
+            justifyContent: 'space-between',
+            paddingHorizontal: 2,
+        },
+        headerButton: {
+            flexShrink: 0,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: 55,
+            width: 55,
+        },
+        headerTitle: {
+            color: 'white',
+            fontSize: 22,
+            fontWeight: 'bold',
+        },
+        leftComponent: {
+            flex: 1,
+        },
+        centerComponent: {
+            height: 55,
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        rightComponent: {
+            flex: 1,
+            alignItems: 'flex-end',
+        },
+    });
+    
     return (
-        <HeaderRNE
-            containerStyle={styles.header}
-            leftComponent={
-                leftChild ? leftChild :
-                <Pressable onPress={() => navigation.navigate('settings')}>
-                    <View style={styles.headerButton}>
-                        <Icon type="antdesign" size={31} name="setting" color="white" />
-                    </View>
-                </Pressable>
-            }
-            rightComponent={
-                rightChild ? rightChild :
-                <Pressable onPress={() => navigation.navigate('addhabit')}>
-                    <View style={styles.headerButton}>
-                        <Icon type="antdesign" size={29} name="plus" color="white" />
-                    </View>
-                </Pressable>
-            }
-            centerComponent={
-            <View style={styles.centerComponent}>
-                <Text style={styles.headerTitle}>Habitty</Text>
-            </View>
-        }
-            ViewComponent={LinearGradient} // Required for gradient
-            linearGradientProps={{
-                colors: ['#7fcbfd', '#3c95d0'],
-                start: { x: 0.2, y: 0.5 },
-                end: { x: 1, y: 0.5 },
-            }}
-        />
-    )
+        <View style={styles.header}>
+        <LinearGradient
+            colors={['#7fcbfd', '#3c95d0']}
+            start={{ x: 0.2, y: 0.5 }}
+            end={{ x: 1, y: 0.5 }}
+            style={styles.header}
+        >
+            <SafeAreaView style={styles.headerContent}>
+                <View style={styles.leftComponent}>
+                    {leftChild ? leftChild : (
+                        <Pressable onPress={() => navigation.navigate('settings')}>
+                            <View style={styles.headerButton}>
+                                <Icon type="antdesign" size={31} name="setting" color="white" />
+                            </View>
+                        </Pressable>
+                    )}
+                </View>
+                <View style={styles.centerComponent}>
+                    <Text style={styles.headerTitle}>Habitty</Text>
+                </View>
+                <View style={styles.rightComponent}>
+                    {rightChild ? rightChild : (
+                        <Pressable onPress={() => navigation.navigate('addhabit')}>
+                            <View style={styles.headerButton}>
+                                <Icon type="antdesign" size={29} name="plus" color="white" />
+                            </View>
+                        </Pressable>
+                    )}
+                </View>
+            </SafeAreaView>
+        </LinearGradient>
+        </View>
+
+    );
 });
-
-const styles = StyleSheet.create({
-    heading: {
-        color: 'white',
-        fontSize: 18,
-        fontWeight: 'bold',
-        alignItems: "center",
-        justifyContent: 'center',
-        
-    },
-    header: {
-        padding: 0,
-        minHeight: 55,
-        paddingVertical: 0,
-        paddingHorizontal: 0,
-        border: "none",
-    },
-    headerButton: {
-    flexShrink: 0,
-        flexDirection: 'row',
-        alignItems: "center",
-        justifyContent: "center",
-        height: 55,
-        width: 55,
-        pointerEvents: "none",
-        userSelect: "none",
-    },
-    headerTitle: {
-        color: "white",
-        fontSize: 22,
-        fontWeight: 'bold',
-        alignItems: "center",
-        justifyContent: "center",
-        pointerEvents: "none",
-        userSelect: "none",
-    },
-
-    centerComponent: {
-        height: 55,
-        justifyContent: "center",
-        alignItems:"center"
-    },
-});
-
-
 
