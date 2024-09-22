e823b0a294392d8126377caafab8a6b11796fceb
P Jaworski <pjaworski.dev@gmail.com>
2024-09-22 11:36:47 +0200

    upd. useHabitScore

--- src/hooks/useHabitScore.js
+++ src/hooks/useHabitScore.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import { REPEAT_GAP_VALUES, REPEAT_MASKS } from '@constants';
+import { REPEAT_GAP_VALUES } from '@constants';
 
 const now = new Date();
 const startOfMonthTS = new Date(now.getFullYear(), now.getMonth(), 1).getTime();
 const endOfMonthTS = new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59, 59, 999).getTime();
-
-
-
-
-
-
+const ONE_DAY_IN_MS = 86400000;
 
 
 export default function useHabitScore(item) {
@@ -19,21 +14,36 @@ export default function useHabitScore(item) {
 
     if (item) {
         const { datesArray, repeat } = item;
-        
+        const tsSorted = Array.from(datesArray).sort((a, b) => b - a);
+
         // score part
+        // gets obj of current preset (repeat count) 
         const GAP_VALUES = REPEAT_GAP_VALUES[repeat]
-        score = datesArray.length * GAP_VALUES.day_percent;
+
+
+        function getLastActivePortion() {
+            const reset_gap = (GAP_VALUES.reset_gap) * ONE_DAY_IN_MS;
+            return Array.from(tsSorted).reduce((acc, cur, i) => {
+                // if first iteration or next timestamp has lower gap detween prev than reset_gap
+                 if (i === 0 || acc?.length && acc[0] - reset_gap <= cur) 
+                    acc.unshift(cur);
+                return acc
+            }, [])
+        }
+
+
+        // sets score
+        score = getLastActivePortion().length * GAP_VALUES.day_percent;
 
 
         // beta
-        const tsSorted = Array.from(datesArray).sort((a, b) => b - a);
         const tsCurrentMonth = Array.from(datesArray)
             .filter(ts => ts >= startOfMonthTS && ts <= endOfMonthTS);
         monthScore = tsSorted.length * GAP_VALUES.day_percent;
 
     }
 
-
+    // fallback
     if (score > 100) score = 100;
     if (monthScore > 100) monthScore = 100;
     if (yearScore > 100) yearScore = 100;
